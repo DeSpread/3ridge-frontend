b4fdeca4102b3fe97764b9321a74a8833aa6b142
cryptogang.btc <cryptodevgang@gmail.com>
2022-12-14 11:41:53 +0900

    revert: recover event page

--- src/__generated__/gql.ts
+++ src/__generated__/gql.ts
@@ -13,9 +13,14 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel-plugin for production.
  */
 const documents = {
+    "\n    query GetUsersQuery {\n        users {\n            username\n        }\n    }\n": types.GetUsersQueryDocument,
     "\n  query GetUsersQuery {\n    users {\n      username\n    }\n  }\n": types.GetUsersQueryDocument,
 };
 
+/**
+ * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function gql(source: "\n    query GetUsersQuery {\n        users {\n            username\n        }\n    }\n"): (typeof documents)["\n    query GetUsersQuery {\n        users {\n            username\n        }\n    }\n"];
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
