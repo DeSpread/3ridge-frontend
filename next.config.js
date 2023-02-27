5ed17cb644ab268014294c91c500fe78e94d2b37
Kim Min Su <kms860226@naver.com>
2023-02-27 13:44:27 +0900

    fix: remove sentry

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 // with Sentry.
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-const { withSentryConfig } = require("@sentry/nextjs");
+// const { withSentryConfig } = require("@sentry/nextjs");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -47,8 +47,8 @@ const nextConfig = {
 
 module.exports = nextConfig;
 
-module.exports = withSentryConfig(
-  module.exports,
-  { silent: true },
-  { hideSourcemaps: true, dryRun: process.env.VERCEL_ENV !== "production" }
-);
+// module.exports = withSentryConfig(
+//   module.exports,
+//   { silent: true },
+//   { hideSourcemaps: true }
+// );
