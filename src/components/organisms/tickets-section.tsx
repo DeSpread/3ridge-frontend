cd164ce024883f8f67ea5c2ca6afc00bd6263b94
Kim Min Su <kms860226@naver.com>
2023-03-09 17:27:47 +0900

    feature: implement tabs trending, newest ...

--- src/components/organisms/tickets-section.tsx
+++ src/components/organisms/tickets-section.tsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  ButtonProps,
   Grid,
   Skeleton,
   Stack,
@@ -19,6 +20,9 @@ import { useLoading } from "../../provider/loading/loading-provider";
 import { MouseEventWithParam, Ticket, TicketEventParam } from "../../type";
 import { useTheme } from "@mui/material/styles";
 import TicketCard from "../molecules/ticket-card";
+import PrimaryButton from "../atoms/primary-button";
+import LocalFireDepartmentIcon from "@mui/icons-material/LocalFireDepartment";
+import QueryBuilderIcon from "@mui/icons-material/QueryBuilder";
 
 type TicketSectionProps = PropsWithChildren & {
   tickets?: Ticket[];
@@ -36,17 +40,138 @@ interface StyledTabProps {
   label: string;
 }
 
+type TabButtonProps = ButtonProps & {
+  index: number;
+  onChange?: MouseEventHandler;
+};
+
+const TabButton = (props: TabButtonProps) => {
+  const theme = useTheme();
+  return (
+    <PrimaryButton
+      {...props}
+      sx={{
+        ":disabled": {
+          backgroundColor: theme.palette.neutral[900],
+          color: theme.palette.neutral[100],
+          borderColor: theme.palette.neutral[100],
+        },
+        background: "transparent",
+        borderColor: "transparent",
+        color: theme.palette.neutral[100],
+        ...props.sx,
+      }}
+      onClick={(e) => {
+        const myEvent = {} as MouseEventWithParam<{ index: number }>;
+        myEvent.params = {
+          index: props.index,
+        };
+        props.onChange?.(myEvent);
+      }}
+      size={props.size}
+    ></PrimaryButton>
+  );
+};
+
 const AntTab = styled((props: StyledTabProps) => (
   <Tab disableRipple {...props} />
 ))(({ theme }) => ({
-  textTransform: "none",
-  marginRight: theme.spacing(1),
+  // textTransform: "none",
+  // marginRight: theme.spacing(1),
   color: "#646176",
+  fontSize: "1.125rem",
   "&.Mui-selected": {
     color: "white",
     fontWeight: theme.typography.fontWeightMedium,
   },
+  // marginLeft: "auto",
 }));
+
+const TabButtonGroup2 = (props: TabButtonGroupProps) => {
+  const theme = useTheme();
+  const mdUp = useMediaQuery(theme.breakpoints.up("md"));
+  const smUp = useMediaQuery(theme.breakpoints.up("sm"));
+
+  const [selectedIdx, setSelectedIdx] = useState(0);
+  const TITLES = [
+    {
+      title: "Trending",
+      icon: (
+        <LocalFireDepartmentIcon
+          sx={{ color: "red" }}
+        ></LocalFireDepartmentIcon>
+      ),
+    },
+    {
+      title: "Newest",
+      icon: <QueryBuilderIcon sx={{ color: "#62cbff" }}></QueryBuilderIcon>,
+    },
+  ];
+  const [anchorEl, setAnchorEl] = useState<Element>();
+  const [open, setOpen] = useState(false);
+  const [selectedMenuIndex, setSelectedMenuIndex] = useState(0);
+  const MENU_ITEMS = ["Recently", "Popular"];
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+    setOpen(true);
+  };
+
+  const handleClose = (index?: number) => {
+    setAnchorEl(undefined);
+    setOpen(false);
+    if (index !== undefined) {
+      console.log(index);
+      setSelectedMenuIndex(index);
+    }
+  };
+
+  return (
+    <Grid
+      container
+      justifyContent={"space-between"}
+      rowSpacing={2}
+      onClick={() => {
+        if (anchorEl) {
+          setAnchorEl(undefined);
+          setOpen(false);
+        }
+      }}
+    >
+      <Grid item>
+        <Stack direction={"row"} spacing={smUp ? 2 : 1}>
+          {TITLES.map((e, index) => {
+            return (
+              <TabButton
+                key={index}
+                sx={{
+                  paddingLeft: 3,
+                  paddingRight: 3,
+                  width: smUp ? 128 : 100,
+                  borderRadius: 1,
+                }}
+                index={index}
+                onChange={(e) => {
+                  // const myEvent = e as MouseEventWithParam<{ index: number }>;
+                  setSelectedIdx(index);
+                  props?.onChange?.(index);
+                }}
+                disabled={selectedIdx === index}
+                size={"small"}
+              >
+                <Stack direction={"row"} alignItems={"center"} spacing={"4px"}>
+                  {e.icon}
+                  <Typography className={"MuiTypography"} fontSize={"1.125rem"}>
+                    {e.title}
+                  </Typography>
+                </Stack>
+              </TabButton>
+            );
+          })}
+        </Stack>
+      </Grid>
+    </Grid>
+  );
+};
 
 const TabButtonGroup = (props: TabButtonGroupProps) => {
   const theme = useTheme();
@@ -81,39 +206,24 @@ const TabButtonGroup = (props: TabButtonGroupProps) => {
   };
 
   return (
-    <Grid
-      container
-      justifyContent={"space-between"}
-      rowSpacing={2}
-      onClick={() => {
-        if (anchorEl) {
-          setAnchorEl(undefined);
-          setOpen(false);
-        }
+    <Tabs
+      value={selectedIdx}
+      onChange={(event: React.SyntheticEvent, newValue: number) => {
+        setSelectedIdx(newValue);
+        props?.onChange?.(newValue);
       }}
+      aria-label="Events"
+      TabIndicatorProps={{
+        style: {
+          backgroundColor: "white", //theme.palette.primary.main,
+        },
+      }}
+      sx={{ background: "" }}
     >
-      <Grid item>
-        <Stack direction={"row"} spacing={smUp ? 2 : 1}>
-          <Tabs
-            value={selectedIdx}
-            onChange={(event: React.SyntheticEvent, newValue: number) => {
-              setSelectedIdx(newValue);
-              props?.onChange?.(newValue);
-            }}
-            aria-label="Events"
-            TabIndicatorProps={{
-              style: {
-                backgroundColor: "white", //theme.palette.primary.main,
-              },
-            }}
-          >
-            <AntTab label={TITLES[0]} {...a11yProps(0)} />
-            <AntTab label={TITLES[1]} {...a11yProps(1)} />
-            <AntTab label={TITLES[2]} {...a11yProps(2)} />
-          </Tabs>
-        </Stack>
-      </Grid>
-    </Grid>
+      <AntTab label={TITLES[0]} {...a11yProps(0)} />
+      <AntTab label={TITLES[1]} {...a11yProps(1)} />
+      <AntTab label={TITLES[2]} {...a11yProps(2)} />
+    </Tabs>
   );
 };
 
@@ -121,6 +231,9 @@ const TicketsSection = (props: TicketSectionProps) => {
   const [tabValue, setTabValue] = useState(0);
   const { showLoading, closeLoading } = useLoading();
   const { tickets, loading, onTicketClick, onTabClick } = props;
+  const theme = useTheme();
+  const smUp = useMediaQuery(theme.breakpoints.up("sm"));
+  const mdUp = useMediaQuery(theme.breakpoints.up("md"));
 
   const onTicketCardClick = (ticket: Ticket) => {
     const myEvent = {} as MouseEventWithParam<TicketEventParam>;
@@ -136,13 +249,46 @@ const TicketsSection = (props: TicketSectionProps) => {
         ...props.sx,
       }}
     >
-      <TabButtonGroup
-        onChange={(e) => {
-          // const myEvent = e as MouseEventWithParam<{ index: number }>;
-          setTabValue(e);
-          onTabClick?.(e);
-        }}
-      ></TabButtonGroup>
+      <Grid
+        container
+        sx={{ background: "" }}
+        direction={smUp ? "row" : "column"}
+        justifyContent={"space-between"}
+        columnSpacing={1}
+        alignItems={smUp ? "center" : "flex-start"}
+      >
+        <Grid item>
+          <TabButtonGroup2></TabButtonGroup2>
+          {/*<Stack direction={"row"} alignItems={"center"} spacing={1}>*/}
+          {/*  {[{ title: "Trending" }, { title: "Newest" }].map((e, index) => {*/}
+          {/*    return (*/}
+          {/*      <PrimaryButton sx={{ borderRadius: "8px" }} key={index}>*/}
+          {/*        <Stack direction={"row"} spacing={"4px"}>*/}
+          {/*          <LocalFireDepartmentIcon*/}
+          {/*            sx={{ color: "red" }}*/}
+          {/*          ></LocalFireDepartmentIcon>*/}
+          {/*          <Typography*/}
+          {/*            className={"MuiTypography"}*/}
+          {/*            sx={{ color: "black" }}*/}
+          {/*          >*/}
+          {/*            Trending*/}
+          {/*          </Typography>*/}
+          {/*        </Stack>*/}
+          {/*      </PrimaryButton>*/}
+          {/*    );*/}
+          {/*  })}*/}
+          {/*</Stack>*/}
+        </Grid>
+        <Grid item>
+          <TabButtonGroup
+            onChange={(e) => {
+              // const myEvent = e as MouseEventWithParam<{ index: number }>;
+              setTabValue(e);
+              onTabClick?.(e);
+            }}
+          ></TabButtonGroup>
+        </Grid>
+      </Grid>
       <Box sx={{ marginTop: 6 }}>
         <Grid container spacing={2}>
           {loading &&
