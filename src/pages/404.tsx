fb7e3ea29eb8a469755ab859fc970b1d3d4f2b3a
Kim Min Su <kms860226@naver.com>
2023-03-09 16:19:44 +0900

    feature: 404 page implement

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,8 +6,11 @@ import {
   Container,
   Typography,
   useMediaQuery,
+  Stack,
 } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
+import Image from "next/image";
+import PrimaryButton from "../components/atoms/primary-button";
 
 const NotFound = () => {
   const theme = useTheme();
@@ -30,54 +33,46 @@ const NotFound = () => {
           display: "flex",
           flexGrow: 1,
           height: "100vh",
-          // py: "80px",
+          justifyContent: "center",
         }}
       >
-        {/*<Container maxWidth="lg">*/}
-        {/*  <Typography align="center" variant={mobileDevice ? "h4" : "h1"}>*/}
-        {/*    404: The page you are looking for isn’t here*/}
-        {/*  </Typography>*/}
-        {/*  <Typography*/}
-        {/*    align="center"*/}
-        {/*    color="textSecondary"*/}
-        {/*    sx={{ mt: 0.5 }}*/}
-        {/*    variant="subtitle2"*/}
-        {/*  >*/}
-        {/*    You either tried some shady route or you came here by mistake.*/}
-        {/*    Whichever it is, try using the navigation.*/}
-        {/*  </Typography>*/}
-        {/*  <Box*/}
-        {/*    sx={{*/}
-        {/*      display: "flex",*/}
-        {/*      justifyContent: "center",*/}
-        {/*      mt: 6,*/}
-        {/*    }}*/}
-        {/*  >*/}
-        {/*    <Box*/}
-        {/*      alt="Under development"*/}
-        {/*      component="img"*/}
-        {/*      src={`/static/error/error404_${theme.palette.mode}.svg`}*/}
-        {/*      sx={{*/}
-        {/*        height: "auto",*/}
-        {/*        maxWidth: "100%",*/}
-        {/*        width: 400,*/}
-        {/*      }}*/}
-        {/*    />*/}
-        {/*  </Box>*/}
-        {/*  <Box*/}
-        {/*    sx={{*/}
-        {/*      display: "flex",*/}
-        {/*      justifyContent: "center",*/}
-        {/*      mt: 6,*/}
-        {/*    }}*/}
-        {/*  >*/}
-        {/*    <NextLink href="/dashboard" passHref>*/}
-        {/*      <Button component="a" variant="outlined">*/}
-        {/*        Back to Dashboard*/}
-        {/*      </Button>*/}
-        {/*    </NextLink>*/}
-        {/*  </Box>*/}
-        {/*</Container>*/}
+        {/*<Box sx={{ height: "100vh" }}>*/}
+        <Stack
+          sx={{ flex: 1, marginTop: -16 }}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <Image
+            alt={""}
+            width={512}
+            height={512}
+            src={"https://3ridge.s3.ap-northeast-2.amazonaws.com/icon/404.svg"}
+            style={{ marginBottom: -64 }}
+          ></Image>
+          <Typography
+            align="center"
+            color="textSecondary"
+            sx={{ mt: 0.5 }}
+            variant="body1"
+          >
+            You either tried some shady route or you came here by mistake.
+            Whichever it is, try using the navigation.
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              mt: 6,
+            }}
+          >
+            <NextLink href="/home" passHref>
+              <PrimaryButton variant="outlined">
+                Back to Dashboard
+              </PrimaryButton>
+            </NextLink>
+          </Box>
+        </Stack>
+        {/*</Box>*/}
       </Box>
     </>
   );
