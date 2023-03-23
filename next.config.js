f59720b8e85bf1a3886c90f18163155963cbedb1
Kim Min Su <kms860226@naver.com>
2023-03-23 12:00:28 +0900

    fix: sentry comment remove

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 // This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-// const { withSentryConfig } = require("@sentry/nextjs");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -29,26 +25,6 @@ const nextConfig = {
       },
     ],
   },
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: "/api/google-auth",
-  //       destination: "https://www.googleapis.com/auth/userinfo.email",
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
-  // if you want to hide url, use this
-  // { source: <api>, destination: <api> }
-  // async rewrites() {
-  //   return [{}];
-  // },
 };
 
 module.exports = nextConfig;
-
-// module.exports = withSentryConfig(
-//   module.exports,
-//   { silent: true },
-//   { hideSourcemaps: true }
-// );
