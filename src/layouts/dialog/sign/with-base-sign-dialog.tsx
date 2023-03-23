5dc13a043243e5f58606a00547baaf34df52524a
Kim Min Su <kms860226@naver.com>
2023-03-23 16:20:30 +0900

    fix: login ui

--- src/layouts/dialog/sign/with-base-sign-dialog.tsx
+++ src/layouts/dialog/sign/with-base-sign-dialog.tsx
@@ -76,32 +76,32 @@ const WithBaseSignInDialog = <P extends SignDialogProps>(
                 direction={"column"}
                 alignItems={"center"}
                 spacing={2}
-                sx={{ width: "100%", height: 320, background: "" }}
+                sx={{ width: "100%", height: 218, background: "" }}
               >
                 <WrappedContent {...props}></WrappedContent>
               </Stack>
               {/*---*/}
-              <Divider sx={{ width: "100%", paddingTop: 0 }}></Divider>
-              <Grid container direction={"row"}>
-                <Grid item>
-                  <Typography variant={"body2"}>
-                    By signing up, you agree to our
-                  </Typography>
-                </Grid>
-                <Grid item>
-                  <LinkTypography variant={"body2"}>
-                    &nbsp;Terms of use
-                  </LinkTypography>
-                </Grid>
-                <Grid item>
-                  <Typography variant={"body2"}>&nbsp;and</Typography>
-                </Grid>
-                <Grid item>
-                  <LinkTypography variant={"body2"}>
-                    Privacy Policy
-                  </LinkTypography>
-                </Grid>
-              </Grid>
+              {/*<Divider sx={{ width: "100%", paddingTop: 0 }}></Divider>*/}
+              {/*<Grid container direction={"row"}>*/}
+              {/*  <Grid item>*/}
+              {/*    <Typography variant={"body2"}>*/}
+              {/*      By signing up, you agree to our*/}
+              {/*    </Typography>*/}
+              {/*  </Grid>*/}
+              {/*  <Grid item>*/}
+              {/*    <LinkTypography variant={"body2"}>*/}
+              {/*      &nbsp;Terms of use*/}
+              {/*    </LinkTypography>*/}
+              {/*  </Grid>*/}
+              {/*  <Grid item>*/}
+              {/*    <Typography variant={"body2"}>&nbsp;and</Typography>*/}
+              {/*  </Grid>*/}
+              {/*  <Grid item>*/}
+              {/*    <LinkTypography variant={"body2"}>*/}
+              {/*      Privacy Policy*/}
+              {/*    </LinkTypography>*/}
+              {/*  </Grid>*/}
+              {/*</Grid>*/}
             </Stack>
           </DialogContent>
         </Dialog>
