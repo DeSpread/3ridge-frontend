e5881e750c33d97d8a0c159db28d5b3e7d385df0
cryptogang.btc <cryptodevgang@gmail.com>
2023-04-28 17:02:51 +0900

    feature: add typo components to banner on home page

--- src/components/organisms/banner-swiper-section.tsx
+++ src/components/organisms/banner-swiper-section.tsx
@@ -8,11 +8,14 @@ import React from "react";
 import SwiperCore, { Navigation } from "swiper";
 import Image from "next/image";
 import PrimaryButton from "../atoms/primary-button";
+import {useRouter} from "next/router";
 
 SwiperCore.use([Navigation]);
 
 const BannerSwiperSection = (props: { width: number | string }) => {
-  return (
+    const router = useRouter();
+
+    return (
     <Stack
       direction={"column"}
       sx={{
@@ -44,78 +47,70 @@ const BannerSwiperSection = (props: { width: number | string }) => {
                   backgroundPosition: "center",
                 }}
               >
-                {/*<div*/}
-                {/*  style={{*/}
-                {/*    position: "absolute",*/}
-                {/*    width: height,*/}
-                {/*    height: height,*/}
-                {/*    background: "",*/}
-                {/*    left: 0,*/}
-                {/*    top: 0,*/}
-                {/*    padding: mdUp ? 24 : smUp ? 10 : 10,*/}
-                {/*  }}*/}
-                {/*>*/}
-                {/*  <Stack direction={"column"} alignItems={"end"}>*/}
-                {/*    <Image*/}
-                {/*      alt={"3ridge-logo"}*/}
-                {/*      width={mdUp ? 150 : smUp ? 140 : 130}*/}
-                {/*      height={mdUp ? 58 : smUp ? 40 : 37}*/}
-                {/*      src={*/}
-                {/*        "https://3ridge.s3.ap-northeast-2.amazonaws.com/logo/02_svg/3ridge_logo_white.svg"*/}
-                {/*      }*/}
-                {/*    />*/}
-                {/*  </Stack>*/}
-                {/*  <Stack*/}
-                {/*    direction={"column"}*/}
-                {/*    sx={{ background: "", width: "100%", height: "100%" }}*/}
-                {/*    paddingTop={mdUp ? 10 : smUp ? 3 : 1}*/}
-                {/*    paddingLeft={mdUp ? 5 : smUp ? 3 : 1}*/}
-                {/*  >*/}
-                {/*    <Stack direction={"column"} alignItems={"left"}>*/}
-                {/*      <Stack direction={"row"} alignItems={"left"} spacing={1}>*/}
-                {/*        <Typography*/}
-                {/*          variant={mdUp ? "h2" : smUp ? "body1" : "body2"}*/}
-                {/*          textAlign={"left"}*/}
-                {/*          sx={{*/}
-                {/*            overflow: "hidden",*/}
-                {/*            textOverflow: "ellipsis",*/}
-                {/*            display: "-webkit-box",*/}
-                {/*            WebkitLineClamp: "3",*/}
-                {/*            WebkitBoxOrient: "vertical",*/}
-                {/*          }}*/}
-                {/*        >*/}
-                {/*          국내 Web3 컨텐츠 플랫폼, 3ridge에서 시작하세요*/}
-                {/*        </Typography>*/}
-                {/*      </Stack>*/}
-                {/*      <Stack*/}
-                {/*        sx={{ marginTop: mdUp ? 4 : 2 }}*/}
-                {/*        alignItems={"left"}*/}
-                {/*      >*/}
-                {/*        <Typography*/}
-                {/*          variant={mdUp ? "h5" : smUp ? "body1" : "body2"}*/}
-                {/*          textAlign={"left"}*/}
-                {/*        >*/}
-                {/*          웹3, 다양한 경험에 함께 참여하세요!*/}
-                {/*        </Typography>*/}
-                {/*      </Stack>*/}
-                {/*      <Stack*/}
-                {/*        direction={"row"}*/}
-                {/*        alignItems={"left"}*/}
-                {/*        spacing={1}*/}
-                {/*        sx={{ marginTop: mdUp ? 8 : smUp ? 5 : 3 }}*/}
-                {/*      >*/}
-                {/*        <PrimaryButton*/}
-                {/*          onClick={async () => {*/}
-                {/*            router.push(`/explore`).then((res) => {});*/}
-                {/*            return;*/}
-                {/*          }}*/}
-                {/*        >*/}
-                {/*          이벤트 참여하기*/}
-                {/*        </PrimaryButton>*/}
-                {/*      </Stack>*/}
-                {/*    </Stack>*/}
-                {/*  </Stack>*/}
-                {/*</div>*/}
+                <Box
+                    style={{
+                        position: "absolute",
+                        width: "100%",
+                        height: "100%",
+                        background: "",
+                        left: 0,
+                        top: 0,
+                        paddingTop: 40,
+                        paddingBottom: 40,
+                        paddingRight: 50,
+                        paddingLeft: 50,
+                    }}
+                >
+                <Stack direction={"column"}
+                       sx={{ width: "100%", height: "100%"}}
+                       justifyContent={"space-between"}>
+                    <Stack direction={"column"} sx={{ width: "100%", height: "100%"}}>
+                        <Stack direction={"column"} sx={{ width: "100%"}}>
+                            <Stack
+                                direction={"row"}
+                                justifyContent={"space-between"}
+                            >
+                                <Stack direction={"column"} alignItems={"flex-start"}>
+                                    <Typography
+                                        variant={"h3"}
+                                        textAlign={"left"}
+                                    >
+                                        국내 Web3 컨텐츠 플랫폼, 3ridge에서 시작하세요
+                                    </Typography>
+                                </Stack>
+                                <Stack direction={"column"} alignItems={"flex-end"}>
+                                    <Image
+                                        alt={"3ridge-logo"}
+                                        width={150}
+                                        height={58}
+                                        src={
+                                            "https://3ridge.s3.ap-northeast-2.amazonaws.com/logo/02_svg/3ridge_logo_white.svg"
+                                        }
+                                    />
+                                </Stack>
+                            </Stack>
+                            <Stack direction={"row"} sx={{ marginTop: 2}}>
+                                <Typography
+                                    variant={"h5"}
+                                    textAlign={"left"}
+                                >
+                                    웹3, 다양한 경험에 함께 참여하세요!
+                                </Typography>
+                            </Stack>
+                        </Stack>
+                    </Stack>
+
+                    <Stack direction={"row"}>
+                        <PrimaryButton
+                            onClick={async () => {
+                                router.push(`/explore`).then((res) => {});
+                                return;
+                            }}>
+                            이벤트 참여하기
+                        </PrimaryButton>
+                    </Stack>
+                </Stack>
+                </Box>
               </Box>
             </SwiperSlide>
           </Swiper>
