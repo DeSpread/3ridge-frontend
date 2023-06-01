6711b10c2ca373e5f5bda79f4c7f61220b9a8ee4
cryptogang.btc <cryptodevgang@gmail.com>
2023-06-01 14:39:29 +0900

    feature: add opengraph with twitter meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -65,6 +65,31 @@ const App = (props: AppPropsWithLayout) => {
   return (
     <>
       <Head>
+        <meta property="og:title" content="3ridge : Web3 온보딩 플랫폼" />
+        <meta property="og:url" content="https://3ridge.io" />
+        <meta
+          property="og:image"
+          content="https://3ridge.s3.ap-northeast-2.amazonaws.com/opengraph/3ridge-opengraph.png"
+        />
+        <meta property="og:image:alt" content="3ridge : Web3 온보딩 플랫폼" />
+        <meta
+          property="og:description"
+          content="여러분의 웹3를 위한 여정, 웹3 온보딩 플랫폼 3ridge에 시작하세요"
+        />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="3ridge : Web3 온보딩 플랫폼" />
+        <meta
+          name="twitter:description"
+          content="여러분의 웹3를 위한 여정, 웹3 온보딩 플랫폼 3ridge에 시작하세요"
+        />
+        <meta name="twitter:site" content="@3ridge_io" />
+        <meta name="twitter:creator" content="@3ridge_io" />
+        <meta
+          name="twitter:image"
+          content="https://3ridge.s3.ap-northeast-2.amazonaws.com/opengraph/3ridge-opengraph.png"
+        />
+
         <script
           dangerouslySetInnerHTML={{
             __html: `
