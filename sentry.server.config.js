7fa17e58a1ecc17a0dac9211c9950992a237b6ac
Kim Min Su <kms860226@naver.com>
2023-06-26 17:05:04 +0900

    feature: add sentry env

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -2,12 +2,15 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://b2de85b2bdca4f5f8cf857c2f91db6f1@o4504728150409216.ingest.sentry.io/4504728151982080',
+  environment: process.env.NEXT_PUBLIC_SENTRY_ENV,
+  dsn:
+    SENTRY_DSN ||
+    "https://b2de85b2bdca4f5f8cf857c2f91db6f1@o4504728150409216.ingest.sentry.io/4504728151982080",
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
