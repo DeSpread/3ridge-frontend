2bda30b955c528f2ebc404ec41ce53dd82707680
2-NOW <kj109888@gmail.com>
2023-09-11 18:17:49 +0900

    feat: Add Tailwind CSS configuration files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import "../styles/tailwindcss.css";
 import "prismjs/themes/prism-tomorrow.css";
 
 import React, { ReactElement, ReactNode, useEffect } from "react";
