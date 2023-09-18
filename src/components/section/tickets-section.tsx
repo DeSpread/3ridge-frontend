17a834e27a8f5d375f34c827b7eaa9f16643dfee
2-NOW <kj109888@gmail.com>
2023-09-18 17:08:20 +0900

    hotfix(tickets-section-old): remove LoadMore component and related code

--- src/components/section/tickets-section.tsx
+++ src/components/section/tickets-section.tsx
@@ -185,44 +185,6 @@ const TabButtonGroup = (props: TabButtonGroupProps) => {
   );
 };
 
-const LoadMoreInner = (
-  props: HTMLAttributes<HTMLDivElement>,
-  ref: ForwardedRef<HTMLDivElement>
-) => {
-  return (
-    <div
-      className="flex justify-center items-center gap-2 my-2"
-      ref={ref}
-      {...props}
-    >
-      {/* spin with tailwindcss */}
-      <svg
-        className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <circle
-          className="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          stroke-width="4"
-        ></circle>
-        <path
-          className="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        ></path>
-      </svg>
-      <label>새로운 이벤트 불러오는 중...</label>
-    </div>
-  );
-};
-
-const LoadMore = forwardRef(LoadMoreInner);
-
 const TicketsSection = ({
   tickets,
   loading,
@@ -233,12 +195,6 @@ const TicketsSection = ({
   onListEnd,
   ...props
 }: TicketSectionProps) => {
-  const loadMoreRef = useRef(null);
-
-  useDetectRef(() => {
-    onListEnd?.();
-  }, loadMoreRef);
-
   const theme = useTheme();
   const smUp = useMediaQuery(theme.breakpoints.up("sm"));
 
@@ -305,11 +261,6 @@ const TicketsSection = ({
                 </Grid>
               );
             })}
-          {!loading && !isLastTicketData && (
-            <Grid item xs={30}>
-              <LoadMore ref={loadMoreRef} />
-            </Grid>
-          )}
           {!loading && (tickets?.length === 0 || !tickets) && (
             <Stack
               direction={"column"}
