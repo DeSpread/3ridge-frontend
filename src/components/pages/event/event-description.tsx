d4cb51e72ca46eb14e4fc1ba7aff010bc9a62036
2-NOW <kj109888@gmail.com>
2023-09-22 10:30:38 +0900

    chore: add comment unused children prop in EventDescription

--- src/components/pages/event/event-description.tsx
+++ src/components/pages/event/event-description.tsx
@@ -1,5 +1,6 @@
 import { Box, Stack, Typography, useMediaQuery } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
+import { PropsWithChildren } from "react";
 
 import ContentMetaDataRenderComponent, {
   MarkDownRenderer,
@@ -11,7 +12,10 @@ interface EventDescriptionProps {
   ticketData?: Ticket;
 }
 
-const EventDescription = ({ ticketData }: EventDescriptionProps) => {
+const EventDescription = ({
+  ticketData,
+}: // FIXME: children prop is not used
+PropsWithChildren<EventDescriptionProps>) => {
   const theme = useTheme();
   const smUp = useMediaQuery(theme.breakpoints.up("sm"));
   const mdUp = useMediaQuery(theme.breakpoints.up("md"));
