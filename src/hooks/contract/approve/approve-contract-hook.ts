719d917ad27402052010669565acfa1c0ee3bffa
Kim Min Su <kms860226@naver.com>
2023-09-24 22:50:55 +0900

    feature: implement approve contract connection

---
+++ src/hooks/contract/approve/approve-contract-hook.ts
@@ -0,0 +1,78 @@
+import { useMemo } from "react";
+import { useContractWrite, useWaitForTransaction } from "wagmi";
+
+import { ChainType } from "@/__generated__/graphql";
+import { AppError } from "@/error/my-error";
+import { useBscApproveContractPrepare } from "@/hooks/contract/approve/prepare/bsc-approve-contract-prepare-hook";
+import { useBscTestNetApproveContractPrepare } from "@/hooks/contract/approve/prepare/bsc-testnet-approve-contract-prepare-hook";
+
+export function useApproveContractHook({
+  chain,
+  amount,
+}: {
+  chain: ChainType;
+  amount: number;
+}) {
+  const selectedContractPrepareHook = useMemo(() => {
+    if (chain === ChainType.BnbTestnet)
+      return useBscTestNetApproveContractPrepare;
+    return useBscApproveContractPrepare;
+  }, [chain]);
+
+  const {
+    config,
+    error: prepareError,
+    isError: isPrepareError,
+    isSuccess: isPrepareSuccess,
+  } = selectedContractPrepareHook({ amount });
+
+  const {
+    data,
+    error: writeError,
+    isError: isWriteError,
+    write,
+    isSuccess: isWriteSuccess,
+  } = useContractWrite(config);
+
+  const { isLoading } = useWaitForTransaction({
+    hash: data?.hash,
+    timeout: 10_000,
+    confirmations: 5,
+  });
+
+  const hash = useMemo(() => {
+    return data?.hash;
+  }, [data]);
+
+  const error = useMemo(() => {
+    if (prepareError) return prepareError;
+    if (writeError) return writeError;
+    return undefined;
+  }, [prepareError, writeError]);
+
+  const isError = useMemo(() => {
+    return isPrepareError || isWriteError;
+  }, [isPrepareError, isWriteError]);
+
+  const isSuccess = useMemo(() => {
+    return isPrepareSuccess && isWriteSuccess;
+  }, [isPrepareSuccess, isWriteSuccess]);
+
+  const runContract = () => {
+    write?.();
+  };
+
+  return {
+    runContract: !isPrepareSuccess
+      ? () => {
+          throw new AppError("Contract Prepare Not Yet");
+        }
+      : runContract,
+    isPrepareSuccess,
+    error,
+    isError,
+    hash,
+    isLoading,
+    isSuccess,
+  };
+}
