cd3b5c2806c057ad306e5a1eb4961a925e64a713
2-NOW <kj109888@gmail.com>
2023-09-25 18:47:31 +0900

    style: reorder CSS classes in LoadMoreInner component

--- src/components/section/tickets-section/components.tsx
+++ src/components/section/tickets-section/components.tsx
@@ -40,16 +40,18 @@ export const TicketsSectionSkeleton = () => {
 
 const LoadMoreInner = (
   props: HTMLAttributes<HTMLDivElement>,
-  ref: ForwardedRef<HTMLDivElement>
+  ref: ForwardedRef<HTMLDivElement>,
 ) => {
+  // FIXME: handle svg asset
+
   return (
     <div
-      className="flex justify-center items-center gap-4 h-32"
+      className="flex h-32 items-center justify-center gap-4"
       ref={ref}
       {...props}
     >
       <svg
-        className="animate-spin h-5 w-5 text-white"
+        className="h-5 w-5 animate-spin text-white"
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
@@ -61,12 +63,12 @@ const LoadMoreInner = (
           r="10"
           stroke="currentColor"
           stroke-width="4"
-        ></circle>
+        />
         <path
           className="opacity-75"
           fill="currentColor"
           d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        ></path>
+        />
       </svg>
       <label>새로운 이벤트 불러오는 중...</label>
     </div>
