cdf5fb4a544f8ace00ffb7b46dc42039fae2375b
2-NOW <kj109888@gmail.com>
2023-09-26 21:00:10 +0900

    refactor: update borderRadius to tailwindcss property

--- src/components/form/_ticket-card/image.tsx
+++ src/components/form/_ticket-card/image.tsx
@@ -7,16 +7,13 @@ interface TicketCardImageProps {
 
 export const TicketCardImage = ({ url }: TicketCardImageProps) => {
   return (
-    <div className="relative flex aspect-square w-full items-center justify-center">
+    <div className="relative flex aspect-square w-full items-center justify-center rounded-lg">
       {url ? (
         <Image
           alt="ticket image"
           src={url}
           fill
-          style={{
-            borderRadius: 4,
-            objectFit: "cover",
-          }}
+          style={{ objectFit: "cover" }}
         />
       ) : (
         // TODO: update skeleton with tailwindcss component
@@ -24,7 +21,6 @@ export const TicketCardImage = ({ url }: TicketCardImageProps) => {
           style={{
             width: "100%",
             height: "100%",
-            borderRadius: 4,
           }}
           animation={"wave"}
           variant={"rounded"}
