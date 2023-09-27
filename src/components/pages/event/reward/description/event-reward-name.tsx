5ff905a5f93b78fad2392a8181ac180cd12fff8e
2-NOW <kj109888@gmail.com>
2023-09-27 17:55:51 +0900

    style: add event reward name outline style

--- src/components/pages/event/reward/description/event-reward-name.tsx
+++ src/components/pages/event/reward/description/event-reward-name.tsx
@@ -14,20 +14,15 @@ const EventRewardName = ({
   return (
     <>
       {ticketData?.rewardPolicy?.context?.rewardName && (
-        <Stack
-          direction={"column"}
-          alignItems={"center"}
-          gap={1}
-          justifyContent={"space-between"}
-        >
-          <Typography variant={"body1"}>리워드</Typography>
-          <Stack>
+        <div className="text-outline flex flex-col items-center gap-2">
+          <div className="text-body1">리워드</div>
+          <div>
             {ComponentHelper.renderMultiLineContentText(
               ticketData?.rewardPolicy?.context?.rewardName ?? "",
-              { variant: "body1", textAlign: "center" }
+              { variant: "body1", textAlign: "center" },
             )}
-          </Stack>
-        </Stack>
+          </div>
+        </div>
       )}
       {(!ticketData || !ticketData?.rewardPolicy?.context?.rewardName) && (
         <Box sx={{ width: "100%", height: 30 }}></Box>
