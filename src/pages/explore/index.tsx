cc180f67cae2fc13fa5aaadf0ec61825f69e3982
2-NOW <kj109888@gmail.com>
2023-09-27 14:43:45 +0900

    refactor: clean up code formatting and imports in explore page

--- src/pages/explore/index.tsx
+++ src/pages/explore/index.tsx
@@ -1,20 +1,17 @@
-import React, { ReactElement } from "react";
-import MainLayout from "../../layouts/main-layout";
-import { AppProps } from "next/app";
 import Head from "next/head";
-import { Box, useMediaQuery } from "@mui/material";
-import { useTheme } from "@mui/material/styles";
-import AllEventsSection from "../../components/section/all-events-section";
-import FeaturedEventsSection from "../../components/section/featured-events-section";
-import { AllEvents } from "../../components/section/all-events";
+import React, { ReactElement } from "react";
 
-const Explore = (props: AppProps) => {
+import { AllEvents } from "@/components/section/_all-events";
+import FeaturedEventsSection from "@/components/section/featured-events-section";
+import MainLayout from "@/layouts/main-layout";
+
+const Explore = () => {
   return (
     <>
       <Head>
         <title>3ridge : Web3 온보딩 플랫폼</title>
       </Head>
-      <div className="px-8 py-12 sm:py-8 flex flex-col gap-8">
+      <div className="flex flex-col gap-8 px-8 py-12 sm:py-8">
         <FeaturedEventsSection />
         <AllEvents />
       </div>
