223e657a64aa636e8ee120793800a216b775c34d
Kim Min Su <kms860226@naver.com>
2023-10-05 18:09:39 +0900

    fix: fix build error

--- src/pages/event/[id].tsx
+++ src/pages/event/[id].tsx
@@ -23,32 +23,31 @@ import {
 import ClickTypography from "../../components/atomic/atoms/click-typhography";
 import LinkTypography from "../../components/atomic/atoms/link-typography";
 import SecondaryButton from "../../components/atomic/atoms/secondary-button";
-import EventRewardPolicy from "../../components/pages/event/reward/event-reward-policy";
-import EventTimeBoard from "../../components/pages/event/event-time-board";
-import EventRewardDescription from "../../components/pages/event/reward/event-reward-description";
 import ButtonWithLoading from "../../components/atomic/molecules/button-with-loading";
 import AgreementDialog from "../../components/dialogs/agreement-dialog";
-import EventParticipants from "../../components/pages/event/event-participants";
-import QuestSurveyDialog from "../../components/dialogs/quest/quest-survey-dialog";
-import TypeHelper from "../../helper/type-helper";
-import StringHelper from "../../helper/string-helper";
-import RouterUtil from "../../util/router-util";
-import { useContractHook } from "../../hooks/contract/contract-hook";
 import ContractLoadingDialog from "../../components/dialogs/contract-loading-dialog";
 import QuestQuizDialog from "../../components/dialogs/quest/quest-quiz-dialog";
+import QuestSurveyDialog from "../../components/dialogs/quest/quest-survey-dialog";
 import SimpleDialog from "../../components/dialogs/simple-dialog";
 import TicketRewardHowToDialog from "../../components/dialogs/ticket-edit/ticket-reward-how-to-dialog";
 import Realistic from "../../components/effects/realistic";
 import EventDateRange from "../../components/pages/event/event-date-range";
 import EventDescription from "../../components/pages/event/event-description";
 import EventImage from "../../components/pages/event/event-image";
+import EventParticipants from "../../components/pages/event/event-participants";
 import EventQuests from "../../components/pages/event/event-quests";
+import EventTimeBoard from "../../components/pages/event/event-time-board";
 import EventTitle from "../../components/pages/event/event-title";
+import EventRewardDescription from "../../components/pages/event/reward/event-reward-description";
+import EventRewardPolicy from "../../components/pages/event/reward/event-reward-policy";
 import {
   APP_ERROR_MESSAGE,
   getErrorMessage,
   getLocaleErrorMessage,
 } from "../../error/my-error";
+import StringHelper from "../../helper/string-helper";
+import TypeHelper from "../../helper/type-helper";
+import { useContractHook } from "../../hooks/contract/contract-hook";
 import { useProfileEditDialog } from "../../hooks/profile-edit-dialog-hook";
 import { useSignDialog } from "../../hooks/sign-dialog-hook";
 import { useSignedUserQuery } from "../../hooks/signed-user-query-hook";
@@ -76,6 +75,7 @@ import {
   VerifyTwitterRetweetQuestContext,
   VerifyVisitWebsiteQuestContext,
 } from "../../types";
+import RouterUtil from "../../util/router-util";
 
 const Event = (props: AppProps) => {
   const { userData } = useSignedUserQuery();
@@ -724,7 +724,6 @@ const Event = (props: AppProps) => {
           if (newWindow) newWindow.opener = null;
           break;
         }
-      }
       }
       if (quest.questGuides?.[0]?.content) {
         openSimpleWarningDialog(quest.questGuides[0]);
