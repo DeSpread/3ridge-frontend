21b984556d2462c187bf1de29e1cbf48811a62a9
Kim Min Su <kms860226@naver.com>
2023-10-30 15:07:22 +0900

    fix: active kakao login

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -257,8 +257,8 @@ const MainLayout = (props: MainLayoutProps) => {
                         }}
                         onClick={(e) => {
                           e.preventDefault();
-                          // setShowSignInDialog(true);
-                          setSignInWithNetworkSelectVisible(true);
+                          setShowSignInDialog(true);
+                          // setSignInWithNetworkSelectVisible(true);
                         }}
                       >
                         지갑 연결
