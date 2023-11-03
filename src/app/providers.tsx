6e1f62147a16881b228a97608d77f499e1583007
LivetOwO <livet@kakao.com>
2023-11-03 18:04:56 +0900

    feat: apply tanstack-query provider to global layout

---
+++ src/app/providers.tsx
@@ -0,0 +1,10 @@
+import { PropsWithChildren, useState } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+export default function Providers({ children }: PropsWithChildren) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
