7abb2b86e61f66eaca7122478c6432533ac38f05
LivetOwO <livet@kakao.com>
2023-11-06 18:12:40 +0900

    feat: remove hook to header

--- src/app/header.layout.tsx
+++ src/app/header.layout.tsx
@@ -1,30 +1,30 @@
-"use client";
-
-import { useMediaQuery, useTheme } from "@mui/material";
 import Image from "next/image";
 
 export default function Header() {
-  const theme = useTheme();
-  const smUp = useMediaQuery(theme.breakpoints.up("sm"));
-
   return (
-    <nav>
-      <Image
-        src={
-          "https://3ridge.s3.ap-northeast-2.amazonaws.com/logo/02_svg/3ridge_logo_white.svg"
-        }
-        height={smUp ? 52 : 48}
-        width={smUp ? 132 : 120}
-        alt={""}
-      />
-      <Image
-        src={
-          "https://3ridge.s3.ap-northeast-2.amazonaws.com/logo/02_svg/beta.svg"
-        }
-        height={smUp ? 30 : 30}
-        width={smUp ? 30 : 30}
-        alt={""}
-      />
+    <nav className="flex h-16 items-center border-0 border-b border-solid border-b-neutral-700 bg-neutral-900 pl-4 pr-6">
+      <div className="flex items-start">
+        <div className="relative aspect-[1/0.393] w-28 sm:w-32">
+          <Image
+            src={
+              "https://3ridge.s3.ap-northeast-2.amazonaws.com/logo/02_svg/3ridge_logo_white.svg"
+            }
+            alt={""}
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <div className="relative aspect-square w-7">
+          <Image
+            src={
+              "https://3ridge.s3.ap-northeast-2.amazonaws.com/logo/02_svg/beta.svg"
+            }
+            alt={""}
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+      </div>
     </nav>
   );
 }
--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -211,24 +211,28 @@ const MainLayout = (props: MainLayoutProps) => {
                 marginLeft: -1,
               }}
             >
-              <Stack direction={"row"} alignItems={"flex-start"}>
-                <Image
-                  src={
-                    "https://3ridge.s3.ap-northeast-2.amazonaws.com/logo/02_svg/3ridge_logo_white.svg"
-                  }
-                  height={smUp ? 52 : 48}
-                  width={smUp ? 132 : 120}
-                  alt={""}
-                />
-                <Image
-                  src={
-                    "https://3ridge.s3.ap-northeast-2.amazonaws.com/logo/02_svg/beta.svg"
-                  }
-                  height={smUp ? 30 : 30}
-                  width={smUp ? 30 : 30}
-                  alt={""}
-                />
-              </Stack>
+              <div className="flex items-start">
+                <div className="relative aspect-[1/0.393] w-28 sm:w-32">
+                  <Image
+                    src={
+                      "https://3ridge.s3.ap-northeast-2.amazonaws.com/logo/02_svg/3ridge_logo_white.svg"
+                    }
+                    alt={""}
+                    layout="fill"
+                    objectFit="contain"
+                  />
+                </div>
+                <div className="relative aspect-square w-7">
+                  <Image
+                    src={
+                      "https://3ridge.s3.ap-northeast-2.amazonaws.com/logo/02_svg/beta.svg"
+                    }
+                    alt={""}
+                    layout="fill"
+                    objectFit="contain"
+                  />
+                </div>
+              </div>
             </Box>
             {!props.disableNavButtonSet &&
               (smUp ? (
