22e171bb80ebb1acd07a633db77c18c8a685b608
LivetOwO <livet@kakao.com>
2023-11-16 16:50:01 +0900

    fix: remove test data to emailForm

--- src/layouts/dialog/sign/signInDialog/emailForm.tsx
+++ src/layouts/dialog/sign/signInDialog/emailForm.tsx
@@ -5,12 +5,8 @@ import EmailAuthCodeForm from "./emailAuthCodeForm";
 import { EmailWithAuthCode } from "./types";
 
 export default function EmailForm() {
-  const [emailWithAuthCode, setEmailWithAuthCode] = useState<EmailWithAuthCode>(
-    {
-      email: "livet@kakao.com",
-      code: "aa",
-    },
-  );
+  const [emailWithAuthCode, setEmailWithAuthCode] =
+    useState<EmailWithAuthCode>();
 
   function handleValidateAuthCode(data: EmailWithAuthCode) {
     setEmailWithAuthCode(data);
