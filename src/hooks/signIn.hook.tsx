8ea0f7eb8be367d5a2c7be30e2297af07ea24ee3
LivetOwO <livet@kakao.com>
2023-11-28 15:16:00 +0900

    feat: add jwt-auth-link for automation auth

--- src/hooks/signIn.hook.tsx
+++ src/hooks/signIn.hook.tsx
@@ -12,79 +12,68 @@ import { userDataState } from "@/lib/recoil";
 export function useSignIn() {
   const setUserData = useSetRecoilState(userDataState);
 
-  const accessToken = useRef<string>();
   const [isSignedIn, setIsSignedIn] = useState(false);
 
   const [getUserByAccessToken] = useLazyQuery(GetUserByAccessTokenDocument);
 
   const [signInByEmailMutation] = useMutation(SignInByEmailDocument);
 
-  const updateUserDataByAccessToken = useCallback(
-    async (token: string) => {
-      const userRes = await getUserByAccessToken({
-        variables: {
-          token: token,
-        },
-      });
+  const updateUserDataByAccessToken = useCallback(async () => {
+    const userRes = await getUserByAccessToken();
 
-      const user = userRes.data?.userByAccessToken;
+    const user = userRes.data?.userByAccessToken;
 
-      if (user) {
-        setIsSignedIn(true);
-        setUserData({
-          _id: user._id ?? undefined,
-          email: user.email ?? undefined,
-          name: user.name ?? undefined,
-          profileImageUrl: user.profileImageUrl ?? undefined,
-          walletAddressInfos: user.wallets?.map((e) => {
-            return {
-              address: e.address,
-              network: TypeHelper.convertToSuppoertedNetwork(e.chain),
-            };
-          }),
-          rewardPoint: user.rewardPoint ?? undefined,
-          userSocial: {
-            twitterId: user.userSocial?.twitterId ?? "",
-            telegramUser: user.userSocial?.telegramUser
-              ? {
-                  authDate: user.userSocial?.telegramUser.authDate ?? 0,
-                  firstName: user.userSocial?.telegramUser.firstName ?? "",
-                  hash: user.userSocial?.telegramUser.hash ?? "",
-                  id: user.userSocial?.telegramUser.id ?? 0,
-                  photoUrl: user.userSocial?.telegramUser.photoUrl ?? "",
-                  username: user.userSocial?.telegramUser.username ?? "",
-                }
-              : undefined,
-          },
-          kakao: user.kakao
+    if (user) {
+      setIsSignedIn(true);
+      setUserData({
+        _id: user._id ?? undefined,
+        email: user.email ?? undefined,
+        name: user.name ?? undefined,
+        profileImageUrl: user.profileImageUrl ?? undefined,
+        walletAddressInfos: user.wallets?.map((e) => {
+          return {
+            address: e.address,
+            network: TypeHelper.convertToSuppoertedNetwork(e.chain),
+          };
+        }),
+        rewardPoint: user.rewardPoint ?? undefined,
+        userSocial: {
+          twitterId: user.userSocial?.twitterId ?? "",
+          telegramUser: user.userSocial?.telegramUser
             ? {
-                id: user.kakao.id,
-                connected_at: user.kakao.connected_at,
-                properties: user.kakao
-                  ? {
-                      profile_image: user.kakao.properties?.profile_image ?? "",
-                      thumbnail_image:
-                        user.kakao.properties?.thumbnail_image ?? "",
-                      nickname: user.kakao.properties?.nickname ?? "",
-                    }
-                  : undefined,
+                authDate: user.userSocial?.telegramUser.authDate ?? 0,
+                firstName: user.userSocial?.telegramUser.firstName ?? "",
+                hash: user.userSocial?.telegramUser.hash ?? "",
+                id: user.userSocial?.telegramUser.id ?? 0,
+                photoUrl: user.userSocial?.telegramUser.photoUrl ?? "",
+                username: user.userSocial?.telegramUser.username ?? "",
               }
             : undefined,
-          discord: user.discord ?? undefined,
-        });
-      } else {
-        setIsSignedIn(false);
-        setUserData({});
-      }
-    },
-    [getUserByAccessToken, setUserData],
-  );
+        },
+        kakao: user.kakao
+          ? {
+              id: user.kakao.id,
+              connected_at: user.kakao.connected_at,
+              properties: user.kakao
+                ? {
+                    profile_image: user.kakao.properties?.profile_image ?? "",
+                    thumbnail_image:
+                      user.kakao.properties?.thumbnail_image ?? "",
+                    nickname: user.kakao.properties?.nickname ?? "",
+                  }
+                : undefined,
+            }
+          : undefined,
+        discord: user.discord ?? undefined,
+      });
+    } else {
+      setIsSignedIn(false);
+      setUserData({});
+    }
+  }, [getUserByAccessToken, setUserData]);
 
   useEffect(() => {
-    accessToken.current = localStorage.getItem("accessToken") ?? undefined;
-    if (accessToken.current) {
-      updateUserDataByAccessToken(accessToken.current);
-    }
+    updateUserDataByAccessToken();
   }, [updateUserDataByAccessToken]);
 
   return {
@@ -97,17 +86,15 @@ export function useSignIn() {
         },
       }).then((res) => {
         const newAccessToken = res.data?.signInByEmail.accessToken;
-        accessToken.current = newAccessToken ?? undefined;
         if (newAccessToken) {
           localStorage.setItem("accessToken", newAccessToken);
-          updateUserDataByAccessToken(newAccessToken);
+          updateUserDataByAccessToken();
         }
         return res;
       });
     },
     async logout() {
       localStorage.removeItem("accessToken");
-      accessToken.current = undefined;
       setUserData({});
     },
   };
