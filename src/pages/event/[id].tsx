3921198d7f47e45b2d5e1ddcca6cb04b8c3f6270
LivetOwO <livet@kakao.com>
2024-01-02 16:16:32 +0900

    feat: active validate VerifyTwitterLinkingRetweet

--- src/pages/event/[id].tsx
+++ src/pages/event/[id].tsx
@@ -884,11 +884,11 @@ const Event = (props: AppProps) => {
         QuestPolicyType.VerifyTwitterLinkingRetweet
       ) {
         if (!showTwitterConnectAlert()) {
-          await asyncCompleteQuestOfUser(ticketData?._id, quest._id ?? "");
-          // await asyncVerifyTwitterLinkingAndRetweetQuest(
-          //   ticketData._id,
-          //   quest._id ?? "",
-          // );
+          // await asyncCompleteQuestOfUser(ticketData?._id, quest._id ?? "");
+          await asyncVerifyTwitterLinkingAndRetweetQuest(
+            ticketData._id,
+            quest._id ?? "",
+          );
           updateVerifyState(index);
         }
         myEvent.params.callback("success");
