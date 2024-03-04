2709f94b655840fb99614fe2850aea0dc44340bc
LivetOwO <livet@kakao.com>
2024-03-04 16:34:37 +0900

    feat: add alert for cannot sign up by email  to login modal

--- src/layouts/dialog/sign/signInDialog/signInTypeSelect.tsx
+++ src/layouts/dialog/sign/signInDialog/signInTypeSelect.tsx
@@ -1,5 +1,5 @@
 import { Wallet } from "@mui/icons-material";
-import { Button } from "@mui/material";
+import { Alert, Button } from "@mui/material";
 
 import EmailRoute from "./emailRoute";
 import { SignInType } from "./types";
@@ -12,6 +12,15 @@ interface SignInTypeSelectProps {
 export default function SignInTypeSelect(props: SignInTypeSelectProps) {
   return (
     <>
+      <div className="mb-5">
+        <Alert severity="info">
+          <strong>신규 이메일 회원가입 불가 안내</strong>
+          <p className="text-sm">
+            현재 이메일 인증 처리량이 많아 인증 메일이 발송이 되지 않고
+            있습니다. 빠르게 정상화 할 수 있도록 하겠습니다.
+          </p>
+        </Alert>
+      </div>
       <EmailRoute onSignIn={props.onSignIn} />
       <hr className="my-5" />
       <Button
