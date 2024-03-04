6ad6d715416971628d03dc2f41b6632219d98954
LivetOwO <livet@kakao.com>
2024-03-05 01:03:58 +0900

    Revert "feat: add alert for cannot sign up by email  to login modal"
    This reverts commit 2709f94b655840fb99614fe2850aea0dc44340bc.

--- src/layouts/dialog/sign/signInDialog/signInTypeSelect.tsx
+++ src/layouts/dialog/sign/signInDialog/signInTypeSelect.tsx
@@ -12,18 +12,6 @@ interface SignInTypeSelectProps {
 export default function SignInTypeSelect(props: SignInTypeSelectProps) {
   return (
     <>
-      <div className="mb-5">
-        <Alert severity="info">
-          <strong>신규 이메일 회원가입 불가 안내</strong>
-          <p className="text-sm">
-            현재 이메일 인증 처리량이 많아 인증 메일이 발송이 되지 않고
-            있습니다. 빠르게 정상화 할 수 있도록 하겠습니다.
-          </p>
-          <p className="text-xs">
-            * 아래 지갑 연결하기 버튼을 통해 로그인하실 수 있습니다.
-          </p>
-        </Alert>
-      </div>
       <EmailRoute onSignIn={props.onSignIn} />
       <hr className="my-5" />
       <Button
